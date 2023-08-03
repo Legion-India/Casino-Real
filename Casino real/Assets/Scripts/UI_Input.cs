bd3c71ef99588eab2887c0082ea4c1a6a994b6b2
Advaita <vedanta3000@gmail.com>
2023-08-03 20:41:37 +0530

    added Input Field

---
+++ Casino real/Assets/Scripts/UI_Input.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class UI_Input : MonoBehaviour
+{
+    private TMP_InputField inputField;
+
+    private void Awake(){
+        inputField = transform.Find("inputField").GetComponent<TMP_InputField>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
